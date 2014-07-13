172ef3de20440bd518fca3073540edc24f7e78b9
Alin Posorovaschi <alinposho@yahoo.com>
2014-07-13 17:43:26 +0100

    Add some more code samples from Chapter 2.2

--- src/chapter2/hierarchical-data.scm
+++ src/chapter2/hierarchical-data.scm
@@ -4,4 +4,42 @@
 (define (squares lst)
 	(map (lambda (x) (* x x)) lst))
 
-(squares one-through-four)
+(define sqrs (squares one-through-four))
+(= sqrs one-through-four)
+one-through-four ;; Notice that the initial list has not been modifies by the squares function
+
+(for-each display one-through-four)
+
+(define x (list 1 2 3))
+(define y (list 4 5 6))
+
+(append x y)
+(cons x y)
+
+(pair? (cons 1 2))
+(= f nil)
+
+;; Exercise 2.32 
+(define (subsets s)
+	(if (null? s)
+		(list (list ))
+		(let ((rest (subsets (cdr s))))
+			(append rest (map (lambda (x) (cons (car s) x)) rest)))))
+
+(subsets (list 1 2 3))
+(cons 1 (list ))
+
+;; Exercise 2.34 
+(define (horner-eval x coefficient-sequence)
+	(accumulate (lambda (this-coeff higher-terms) )
+							0
+							coefficient-sequence))
+
+
+;; Nested mapping
+(accumulate append
+						nil
+						(map (lambda (i)
+							(map (lambda (j) (list i j))
+		  						 (enumerate-interval 1 (- i 1))))
+							(enumerate-interval 1 n)))
