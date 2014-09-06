1d18ef1f07cd96ef886fb98a12222689fcccbce7
Alin Posorovaschi <alinposho@yahoo.com>
2014-09-06 18:57:51 +0100

    Finished chapter 4 section 1.4 but no luck running the evaluator
     - Exception ;Unknown procedure type -- APPLY #[compiled-procedure 12 ("list" #x5) #xc #x3219b8] keeps popping up.

--- src/chapter4/the_metacircular_evaluator.scm
+++ src/chapter4/the_metacircular_evaluator.scm
@@ -73,6 +73,12 @@
 												env)
 	'ok)
 
+(define (eval-definition exp env)
+	(define-variable! (definition-variable exp)
+										(eval (definition-value exp) env)
+										env)
+	'ok)
+
 
 ;;; 4.1.2 Representing Expressions
 
@@ -189,11 +195,138 @@
 	(eq? x false))
 
 
+;; Procedures
+(define (make-procedure parameters body env)
+	(list 'procedure parameters body env))
+(define (compound-procedure? p)
+	(tagged-list? p 'procedure))
+(define (procedure-parameters p) (cadr p))
+(define (procedure-body p) (caddr p))
+(define (procedure-environment p) (cadddr p))
 
 
+;; Operations on environments
+(define (enclosing-environment env) (cdr env))
+(define (first-frame env) (car env))
+(define the-empty-environment '())
+
+(define (make-frame variables values)
+	(cons variables values))
+(define (frame-variables frame) (car frame))
+(define (frame-values frame) (cdr frame))
+(define (add-binding-to-frame! var val frame)
+	(set-car! frame (cons var (car frame)))
+	(set-cdr! frame (cons val (cdr frame))))
 
 
+(define (extend-environment vars vals base-env)
+	(if (= (length vars) (length vals))
+			(cons (make-frame vars vals) base-env)
+			(if (< (length vars) (length vals))
+					(error "Too many arguments supplied" vars vals)
+					(error "Too few arguments supplied" vars vals))))
+
+(define (lookup-variable-value var env)
+	(define (env-loop env)
+		(define (scan vars vals)
+			(cond ((null? vars)
+						(env-loop (enclosing-environment env)))
+						((eq? var (car vars)) (car vals))
+						(else (scan (cdr vars) (cdr vals)))))
+		(if (eq? env the-empty-environment)
+				(error "Unbound variable" var)
+				(let ((frame (first-frame env)))
+					(scan (frame-variables frame) (frame-values frame)))))
+	(env-loop env))
 
 
+(define (set-variable-value! var val env)
+	(define (env-loop env)
+		(define (scan vars vals)
+			(cond ((null? vars)
+						(env-loop (enclosing-environment env)))
+						((eq? var (car vars))
+						(set-car! vals val))
+						(else (scan (cdr vars) (cdr vals)))))
+		(if (eq? env the-empty-environment)
+				(error "Unbound variable -- SET!" var)
+				(let ((frame (first-frame env)))
+					(scan (frame-variables frame) (frame-values frame)))))
+	(env-loop env))
+
+(define (define-variable! var val env)
+	(let ((frame (first-frame env)))
+		(define (scan vars vals)
+			(cond ((null? vars)
+						(add-binding-to-frame! var val frame))
+						((eq? var (car vars))
+						(set-car! vals val))
+						(else (scan (cdr vars) (cdr vals)))))
+		(scan (frame-variables frame)
+			(frame-values frame))))
+
+;;; 4.1.4 Running the Evaluator as a Program
+
+(define (primitive-procedure? proc)
+	(tagged-list? proc 'primitive))
+(define (primitive-implementation proc) (cadr proc))
+
+(define primitive-procedures
+	(list (list 'car car)
+				(list 'cdr cdr)
+				(list 'cons cons)
+				(list 'null? null?)
+				(list '+ +)
+				;;<more primitives>
+				))
+
+(define (primitive-procedure-names)
+	(map car primitive-procedures))
+(define (primitive-procedure-objects)
+	(map (lambda (proc) (list 'primitive (cadr proc))) primitive-procedures))
+
+(define (apply-primitive-procedure proc args)
+	(apply-in-underlying-scheme
+		(primitive-implementation proc) args))
+
+(define apply-in-underlying-scheme apply)
+
+(define (setup-environment)
+	(let ((initial-env
+					(extend-environment (primitive-procedure-names)
+															(primitive-procedure-objects)
+															the-empty-environment)))
+		(define-variable! 'true true initial-env)
+		(define-variable! 'false false initial-env)
+		initial-env))
+(define the-global-environment (setup-environment))
+
+(define input-prompt ";;; M-Eval input:")
+(define output-prompt ";;; M-Eval value:")
+(define (driver-loop)
+	(prompt-for-input input-prompt)
+	(let ((input (read)))
+		(let ((output (eval input the-global-environment)))
+			(announce-output output-prompt)
+			(user-print output)))
+	(driver-loop))
+(define (prompt-for-input string)
+	(newline) (newline) (display string) (newline))
+
+(define (announce-output string)
+	(newline) (display string) (newline))
+
+(define (user-print object)
+	(if (compound-procedure? object)
+			(display (list 'compound-procedure
+										(procedure-parameters object)
+										(procedure-body object)
+										'<procedure-env>))
+			(display object)))
+
+(define the-global-environment (setup-environment))
+
+;; Call this to run the metacircular evaluator
+(driver-loop)
 
 
